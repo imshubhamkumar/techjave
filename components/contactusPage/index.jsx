b833ac07271f5358350349070c14e13104a503b2
imshubhamkumar <shubhamk773@gmail.com>
2021-09-12 20:16:05 +0530

    contact us page added

--- components/contactusPage/index.jsx
+++ components/contactusPage/index.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
 import HeroSection from "../heroSection";
+import { contact } from "../../data/contact";
 
 const ContactUsPage = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+   const handleSubmit = (event) => {
+        event.preventDefault()
+        //other stuff
+        sendMessage()
+      }
+   const sendMessage = () => {
+        const formData = new FormData()
+        formData.append(contact.GOOGLE_FORM_NAME_ID, name)
+        formData.append(contact.GOOGLE_FORM_EMAIL_ID, email)
+        formData.append(contact.GOOGLE_FORM_PHONE_ID, phone)
+        formData.append(contact.GOOGLE_FORM_MESSAGE_ID, message)
+        axios.post(contact.GOOGLE_FORM_ACTION_URL, formData)
+         .then((res) => {
+           console.log(res);
+        }).catch((error) => {
+           console.log(error);
+        })
+      }
   return (
     <>
       <HeroSection imgUrl="" title="Contact Us" />
       <div className="container-contact100">
         <div className="wrap-contact100">
-          <form className="contact100-form validate-form">
+          <form 
+          className="contact100-form validate-form" onSubmit={(e) => handleSubmit(e)}>
             <span className="contact100-form-title">Send Us A Message</span>
 
             <div
@@ -16,7 +42,8 @@ const ContactUsPage = () => {
               <input
                 className="input100"
                 type="text"
-                name="name"
+                onChange={(e) => setName(e.target.value)}
+                name="entry.2005620554"
                 placeholder="Full Name"
               />
               <span className="focus-input100"></span>
@@ -29,7 +56,8 @@ const ContactUsPage = () => {
               <input
                 className="input100"
                 type="text"
-                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                name="entry.1045781291"
                 placeholder="E-mail"
               />
               <span className="focus-input100"></span>
@@ -42,7 +70,8 @@ const ContactUsPage = () => {
               <input
                 className="input100"
                 type="text"
-                name="phone"
+                onChange={(e) => setPhone(e.target.value)}
+                name="entry.1166974658"
                 placeholder="Phone"
               />
               <span className="focus-input100"></span>
@@ -54,7 +83,8 @@ const ContactUsPage = () => {
             >
               <textarea
                 className="input100"
-                name="message"
+                name="entry.839337160"
+                onChange={(e) => setMessage(e.target.value)}
                 placeholder="Your Message"
               ></textarea>
               <span className="focus-input100"></span>
---
+++ data/contact.js
@@ -0,0 +1,7 @@
+export const contact ={
+    GOOGLE_FORM_NAME_ID: "entry.2005620554",
+    GOOGLE_FORM_EMAIL_ID: 'entry.1045781291',
+    GOOGLE_FORM_PHONE_ID: 'entry.1166974658',
+    GOOGLE_FORM_MESSAGE_ID: 'entry.839337160',
+    GOOGLE_FORM_ACTION_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdg-KM0g-BtTVHOXX2FDTcvIXD3pbw2hi3d9ZnShiY55AJulA/formResponse'
+}
