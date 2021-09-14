44b6a10e7290b63f93e7a2cec60484674fecf56c
imshubhamkumar <shubhamk773@gmail.com>
2021-09-14 21:11:53 +0530

    color changed

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 					</div>
 					<ul className="navbar-nav ml-auto align-items-center">
 						<li className="nav-item">
-							<a className="nav-link" href="#home">Home <span className="sr-only">(current)</span></a>
+							<a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
 						</li>
 						<li className="nav-item dropdown" onMouseEnter={() => setServiceDropDown(true)} onMouseLeave={() => setServiceDropDown(false)}>
 							<a className={['nav-link', serviceDropDown ? 'show': ''].join(' ')} href="#services" onClick={(e) => e.preventDefault()}>Services</a>
